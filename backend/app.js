a5bbd903eedfa51563220df577553a471898d8d1
Jesen Tanadi <mail@jesentanadi.com>
2020-03-22 16:08:07 -0400

    redirect to https

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ app.use("/static", express.static(path.join(__dirname, "../frontend/static")))
 app.use("/", express.static(path.join(__dirname, "../frontend/static")))
 
 app.get("/", (req, res) => {
+  if (req.hostname !== "localhost" && req.protocol === "http") {
+    res.redirect(`https://${req.get("host")}${req.url}`)
+  }
+
   res.sendFile(path.join(__dirname, "../frontend/index.html"))
 })
 
