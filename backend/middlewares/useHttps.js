311b11a66e3e315cc81aece4a8991964fbe15afe
Jesen Tanadi <mail@jesentanadi.com>
2020-03-22 16:31:39 -0400

    add https middleware

---
+++ backend/middlewares/useHttps.js
@@ -0,0 +1,11 @@
+module.exports = (req, res, next) => {
+  const forwardedProto = req.headers["x-forwarded-proto"] || ""
+  const isSecure = req.secure || forwardedProto === "https"
+
+  if (req.hostname === "localhost" || isSecure) {
+    next()
+  }
+
+  const httpsUrl = `https://${req.get("host")}${req.originalUrl}`
+  res.redirect(301, httpsUrl)
+}
