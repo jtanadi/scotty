d7fc69edb82cdc89de57e9e5ac4d3d5254148e53
Jesen Tanadi <mail@jesentanadi.com>
2020-03-23 00:23:45 -0400

    minor clean up, add react-hot-loader

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
+import "react-hot-loader/patch"
+
 import React from "react"
 import ReactDOM from "react-dom"
 
 import App from "./components/App"
 
-const root = document.getElementById("root")
-
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, document.getElementById("root"))
