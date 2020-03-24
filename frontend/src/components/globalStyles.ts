02e5223447b33283d358a09fb54e63200c0cc876
Jesen Tanadi <mail@jesentanadi.com>
2020-03-24 07:34:16 -0400

    update width and height

--- frontend/src/components/globalStyles.ts
+++ frontend/src/components/globalStyles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components"
 
 const Background = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
   margin: 0 auto;
   background-color: white;
   display: flex;
