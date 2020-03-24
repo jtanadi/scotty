3d03bd47273b98b005a9ade5b12e0e42c28eeb6b
Jesen Tanadi <mail@jesentanadi.com>
2020-03-24 06:54:01 -0400

    move Background to globals

---
+++ frontend/src/components/globalStyles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: white;
+  display: flex;
+`
+
+export { Background }
