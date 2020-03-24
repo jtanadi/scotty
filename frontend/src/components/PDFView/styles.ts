eaf6fc26230ac0ca8faa6e27d8b38fd2551fb8cb
Jesen Tanadi <mail@jesentanadi.com>
2020-03-24 09:31:25 -0400

    add left and right padding

--- frontend/src/components/PDFView/styles.ts
+++ frontend/src/components/PDFView/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 const DocumentContainer = styled.div`
   margin: 0 auto;
-  padding: 4.5rem 0 2rem 0;
+  padding: 4.5rem 2rem 2rem 2rem;
 `
 
 export { DocumentContainer }
