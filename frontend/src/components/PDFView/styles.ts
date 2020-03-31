484970e6bb12e0f1fd912b5a937d04d8a00686b3
Jesen Tanadi <mail@jesentanadi.com>
2020-03-31 14:12:04 -0400

    add shadow around document

--- frontend/src/components/PDFView/styles.ts
+++ frontend/src/components/PDFView/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components"
 const DocumentContainer = styled.div`
   margin: 0 auto;
   padding: 4.5rem 2rem 2rem 2rem;
+  box-shadow: 0 0 1.5rem rgba(0, 0, 0, 0.25);
 `
 
 export { DocumentContainer }
