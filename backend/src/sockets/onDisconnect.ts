2b53ad0a473a08988964423e90595244eafe8b64
Jesen Tanadi <mail@jesentanadi.com>
2020-03-30 22:31:54 -0400

    rename variables, add types

--- backend/src/sockets/onDisconnect.ts
+++ backend/src/sockets/onDisconnect.ts
@@ -1,22 +1,20 @@
 import s3 from "../utils/s3"
-import { rooms, users } from "./cache"
+import { rooms, usersMap } from "./cache"
 
-import { Connection } from "./types"
+import { Connection, User } from "./types"
 
 export default (connection: Connection): void => {
   const { io, socket } = connection
 
-  const roomID = users[socket.id]
+  const roomID = usersMap[socket.id]
   if (!roomID) return
 
   const room = rooms[roomID]
-  room.userIDs = room.userIDs.filter((id: string) => id !== socket.id)
-
-  const participants = room.userIDs
+  room.users = room.users.filter((user: User) => user.id !== socket.id)
 
   // When room is empty, delete object from S3 bucket
   // and clear the associated value in our cache
-  if (!participants.length) {
+  if (!room.users.length) {
     const { pdfUrl } = rooms[roomID]
     s3.deleteObject(
       { Bucket: process.env.S3_BUCKET, Key: pdfUrl },
@@ -32,6 +30,6 @@ export default (connection: Connection): void => {
     rooms[roomID] = null
   }
 
-  users[socket.id] = null
-  io.to(roomID).emit("update participants", { participants })
+  usersMap[socket.id] = null
+  io.to(roomID).emit("update users", { users: room.users })
 }
