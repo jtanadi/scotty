8aca99173be5b34761075251a2aa3b199dbfa617
Jesen Tanadi <mail@jesentanadi.com>
2020-04-01 18:38:38 -0400

    fix small socket bug: add url that points to back end server in development

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,5 +1,12 @@
 import io from "socket.io-client"
 
-const socket = io()
+let socket: SocketIOClient.Socket
+if (process.env.NODE_ENV === "production") {
+  socket = io()
+} else {
+  // Default back end port
+  const PORT = process.env.PORT || 3030
+  socket = io(`http://localhost:${PORT}`)
+}
 
 export default socket
