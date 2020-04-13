62a10aa3ee68ef4b747d3cdcd4c848f97ef0efa8
Jesen Tanadi <mail@jesentanadi.com>
2020-04-13 07:31:15 -0400

    get hostID from socket, and send private message to host when room is created; keep track of number of pages

--- backend/src/sockets/onCreateRoom.ts
+++ backend/src/sockets/onCreateRoom.ts
@@ -2,18 +2,19 @@ import { rooms } from "./cache"
 import { Connection, CreateRoomData, Room, RoomData } from "./types"
 
 export default (connection: Connection, data: CreateRoomData): void => {
-  const { io, socket } = connection
-  const { roomID, pdfUrl } = data
+  const { io } = connection
+  const { hostID, roomID, pdfUrl, numPages } = data
 
   const newRoom: Room = {
     users: [],
     pdfUrl,
     pageNum: 1,
+    numPages,
   }
 
   rooms[roomID] = newRoom
 
   // Send private message back to room creator with roomID
   const roomCreatedData: RoomData = { roomID }
-  io.to(socket.id).emit("room created", roomCreatedData)
+  io.to(hostID).emit("room created", roomCreatedData)
 }
