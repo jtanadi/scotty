dbe0399e9d1fff7945c4260cb02a35bdd8ee7e5d
Jesen Tanadi <mail@jesentanadi.com>
2020-04-13 07:32:14 -0400

    keep track of number of pages

--- backend/src/sockets/onJoinRoom.ts
+++ backend/src/sockets/onJoinRoom.ts
@@ -29,8 +29,9 @@ export default (connection: Connection, data: RoomData): void => {
   // Make sure we're looking at the same doc & page when joining
   // and send userID to client
   const pdfUrl = room.pdfUrl || ""
+  const numPages = room.numPages
 
-  const syncDocData: SyncDocData = { pdfUrl, userID: socket.id }
+  const syncDocData: SyncDocData = { pdfUrl, userID: socket.id, numPages }
   io.to(socket.id).emit("sync document", syncDocData)
 
   const pageNum = room.pageNum || 1
