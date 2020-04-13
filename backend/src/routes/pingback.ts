3cfc24e1ae5db0829ce5ac0ce5d74a40ea620706
Jesen Tanadi <mail@jesentanadi.com>
2020-04-13 08:19:32 -0400

    remove cors

--- backend/src/routes/pingback.ts
+++ backend/src/routes/pingback.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express"
 import io from "socket.io-client"
-import cors from "cors"
 
 import { CreateRoomData } from "../sockets/types"
 
 const router = express.Router()
-router.use(cors())
 
 let socket: SocketIOClient.Socket
 if (process.env.NODE_ENV === "production") {
