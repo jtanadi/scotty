5efa93b12c607fbc4669969a06b4777bba1238b1
Jesen Tanadi <mail@jesentanadi.com>
2020-04-14 09:31:19 -0400

    wrap upload in try/catch; await on conveyor ping

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -41,28 +41,42 @@ const Home: React.FC<{}> = (): ReactElement => {
 
     const uuidv4 = v4()
     const { type } = pdfFile
-    const { message } = (
-      await axios.post(`${conveyorAPI}/convert/pdf?out=png`, pdfFile, {
-        headers: {
-          "Content-Type": type,
-          "x-Pingback": pingbackAddress,
-          "x-Forward-Data": JSON.stringify({
-            hostID: socket.id,
-            roomID: uuidv4,
-          }),
-        },
-      })
-    ).data
 
-    if (!message) {
-      console.error("Message not received")
+    try {
+      const { message } = (
+        await axios.post(`${conveyorAPI}/convert/pdf?out=png`, pdfFile, {
+          headers: {
+            "Content-Type": type,
+            "x-Pingback": pingbackAddress,
+            "x-Forward-Data": JSON.stringify({
+              hostID: socket.id,
+              roomID: uuidv4,
+            }),
+          },
+        })
+      ).data
+
+      if (!message) {
+        console.error("Conveyor error: no message received")
+      }
+    } catch (err) {
+      console.error(`Conveyor error: ${err.message}`)
     }
   }
 
   const [roomID, setRoomID] = useState("")
   useEffect(() => {
-    // Ping conveyor on start, but don't wait for answer
-    axios.get(`${conveyorAPI}/ping`)
+    const pingConveyor = async (): Promise<void> => {
+      try {
+        // Ping conveyor to make sure it's awake
+        const { message } = (await axios.get(`${conveyorAPI}/ping`)).data
+        console.log(`Conveyor status: ${message}`)
+      } catch (err) {
+        console.error("Conveyor not awake")
+      }
+    }
+
+    pingConveyor()
 
     socket.on("room created", (data: RoomData) => {
       setRoomID(data.roomID)
