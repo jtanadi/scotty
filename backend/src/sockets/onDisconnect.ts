dc966bdbe5468a1c614d68efa6e26e76bcdec373
Jesen Tanadi <mail@jesentanadi.com>
2020-04-13 21:15:44 -0400

    delete pages on disconnect

--- backend/src/sockets/onDisconnect.ts
+++ backend/src/sockets/onDisconnect.ts
@@ -17,17 +17,22 @@ export default (connection: Connection): void => {
   // When room is empty, delete object from S3 bucket
   // and clear the associated value in our cache
   if (!room.users.length) {
-    const { pdfUrl } = rooms[roomID]
-    s3.deleteObject(
-      { Bucket: process.env.S3_BUCKET, Key: pdfUrl },
-      (err, data) => {
-        if (err) {
-          console.error("ERROR", err)
-        } else {
-          console.log("DATA", data)
-        }
+    const { pdfUrl, pages } = rooms[roomID]
+
+    const params = {
+      Bucket: process.env.S3_BUCKET,
+      Delete: {
+        Objects: pages.map(page => ({ Key: `${pdfUrl}/${page}` })),
+      },
+    }
+
+    s3.deleteObjects(params, (err, data) => {
+      if (err) {
+        console.error("ERROR", err)
+      } else {
+        console.log("DATA", data)
       }
-    )
+    })
 
     rooms[roomID] = null
   }
