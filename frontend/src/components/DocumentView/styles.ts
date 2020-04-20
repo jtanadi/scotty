399c361a3e27fa1dcc47a516f1645cd4b9276846
Jesen Tanadi <mail@jesentanadi.com>
2020-04-20 19:23:34 -0400

    allow panning

--- frontend/src/components/DocumentView/styles.ts
+++ frontend/src/components/DocumentView/styles.ts
@@ -1,18 +1,40 @@
 import styled from "styled-components"
 
-export const DocumentContainer = styled.div`
-  margin: 0 auto;
-  padding: 4.5rem 2rem 2rem 2rem;
+type ContainerPropType = {
+  scale: number
+}
+
+export const DocumentContainer = styled.div<ContainerPropType>`
+  margin: 2.5rem auto 0 auto;
+  padding: 2rem 0;
   height: calc(100vh - 6.5rem);
-  width: calc(100vw - 4rem);
+  width: 100vw;
   display: flex;
+  overflow: ${(props): string => (props.scale <= 1 ? "hidden" : "scroll")};
 `
 
-export const Page = styled.img`
+type PagePropType = {
+  scale: number
+  mouseDown: boolean
+}
+
+export const Page = styled.img.attrs((props: PagePropType) => {
+  const height = `${props.scale * 100}%`
+
+  let cursor = "default"
+  if (props.scale > 1) {
+    cursor = props.mouseDown ? "grabbing" : "grab"
+  }
+
+  return {
+    style: {
+      height,
+      cursor,
+    },
+  }
+})<PagePropType>`
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.25);
   margin: auto;
-  max-width: 100%;
-  max-height: 100%;
   object-fit: contain;
   background-color: white;
 `
