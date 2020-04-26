43366189cbfaab4f22eceaa96630b0b1b085e7db
Jesen Tanadi <mail@jesentanadi.com>
2020-04-26 15:12:28 -0400

    scale PageContainer instead of scaling Page; use overflow: scroll by default

--- frontend/src/components/DocumentView/styles.ts
+++ frontend/src/components/DocumentView/styles.ts
@@ -1,31 +1,22 @@
 import styled from "styled-components"
 
-type ContainerPropType = {
+type ContainerProp = {
   scale: number
 }
 
-export const DocumentContainer = styled.div<ContainerPropType>`
+export const DocumentContainer = styled.div<ContainerProp>`
   margin: 3rem auto 0 auto;
   height: calc(100vh - 3rem);
   width: 100vw;
-  display: flex;
-  overflow: ${(props): string => (props.scale <= 1 ? "hidden" : "scroll")};
+  overflow: scroll;
 `
 
-export const PageContainer = styled.div`
-  box-sizing: border-box;
-  margin: auto;
-  padding: 2rem;
-  height: 100%;
-  display: flex;
-`
-
-type PagePropType = {
+type PageContainerProp = {
   scale: number
   mouseDown: boolean
 }
 
-export const Page = styled.img.attrs((props: PagePropType) => {
+export const PageContainer = styled.div.attrs((props: PageContainerProp) => {
   const transform = `scale(${props.scale})`
 
   let cursor = "default"
@@ -39,12 +30,20 @@ export const Page = styled.img.attrs((props: PagePropType) => {
       transform,
     },
   }
-})<PagePropType>`
+})<PageContainerProp>`
+  box-sizing: border-box;
+  margin: auto;
+  padding: 2rem;
+  height: 100%;
+  display: flex;
+  transform-origin: top left;
+`
+
+export const Page = styled.img`
   margin: auto;
   max-width: 100%;
   max-height: 100%;
   object-fit: contain;
   background-color: white;
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.25);
-  transform-origin: top left;
 `
