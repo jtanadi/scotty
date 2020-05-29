853a435be71646d9f0f0a409c2926bf5620d016c
Jesen Tanadi <mail@jesentanadi.com>
2020-05-29 09:17:42 -0400

    update styles

--- frontend/src/components/LinkModal/styles.ts
+++ frontend/src/components/LinkModal/styles.ts
@@ -1,48 +1,62 @@
 import styled from "styled-components"
 
-import { H2, H3, P, ButtonOK } from "../globalStyles"
+import { H2, P, Button, COLORS } from "../globalStyles"
+
+export const InputContainer = styled.div`
+  width: 100%;
+  margin: 1.25rem 0.5rem 1.75rem 0;
+  position: relative;
+  height: 2rem;
+`
 
 export const Input = styled.input`
   box-sizing: border-box;
-  width: 100%;
-  height: 2rem;
-  margin: 0.5rem 0;
+  width: calc(100% - 2.25rem - 0.5rem);
+  height: 2.25rem;
+  margin-right: 0.5rem;
   font-family: sans-serif;
   font-size: 0.85rem;
+  position: absolute;
+  top: 0;
+  left: 0;
 
   &:focus {
-    border: 2px solid #56aab8;
+    border: 2px solid ${COLORS.MID_GRAY};
   }
 `
 
-export const ButtonsContainer = styled.div`
+export const CopyButton = styled.button`
+  box-sizing: border-box;
+  width: 2.25rem;
+  height: 2.25rem;
+  border: 0;
+  background-color: ${COLORS.LIGHT_GRAY};
+  border: 1px solid ${COLORS.MID_GRAY};
+  padding: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-image: url(/static/icons/copyPasteboard.svg);
+
+  &:hover {
+    background-color: ${COLORS.MID_GRAY};
+  }
+
+  &:active {
+    background-color: ${COLORS.DARK_GRAY};
+    background-image: url(/static/icons/copyPasteboardLight.svg);
+  }
+`
+
+export const OKButton = styled(Button)`
   width: 100%;
-  display: flex;
-  margin: 1rem 0 1.5rem 0;
-`
-
-export const OKButton = styled(ButtonOK)`
-  flex: 1;
-`
-
-export const HR = styled.hr`
-  margin: 2rem 0 1rem 0;
-  border: 1px solid lightgray;
 `
 
 export const Title = styled(H2)`
   margin: 0;
-`
-
-export const Subtitle = styled(H3)`
-  margin: 0;
+  text-align: center;
 `
 
 export const Body = styled(P)`
-  margin-top: 0.5rem;
-`
-
-export const QuoteAttr = styled.span`
-  font-size: 0.75rem;
-  font-weight: normal;
+  margin: 1.125rem 0 0 0;
 `
