f35c345e304e9b57a55f3043a5d61d87d7850a26
Jesen Tanadi <mail@jesentanadi.com>
2020-06-13 17:19:04 -0400

    move tools to own module

---
+++ frontend/src/utils/tools.ts
@@ -0,0 +1,17 @@
+export type Tool = {
+  name: string
+  image: string
+  hover?: string
+  active?: string
+}
+
+const createTool = (name: string): Tool => ({
+  name,
+  image: `/static/icons/${name}.svg`,
+  hover: `/static/icons/${name}Light.svg`,
+})
+
+/* const tools: Tool[] = ["pointer", "draw", "erase"].map(tool => createTool(tool)) */
+const tools: Tool[] = ["pointer"].map(tool => createTool(tool))
+
+export default tools
