4bbed06c266119796be6519bf811b98849537097
Jesen Tanadi <mail@jesentanadi.com>
2020-06-18 08:35:58 -0400

    load next 2 pages if not cached

--- frontend/src/components/DocumentView/index.tsx
+++ frontend/src/components/DocumentView/index.tsx
@@ -1,18 +1,25 @@
-import React, { FC, ReactElement, RefObject, useRef } from "react"
+import React, { FC, ReactElement, RefObject, useRef, useEffect } from "react"
 import { connect } from "react-redux"
+import { Dispatch } from "redux"
 
 import { DocumentContainer, PageContainer, Page } from "./styles"
-
 import usePanhandler from "./hooks/usePanhandler"
+import { cachePage } from "../../store/actions"
+import { PageCache } from "../../store/types"
 
 type PropTypes = {
   pageRef: RefObject<HTMLImageElement>
 }
 
-const View: FC<PropTypes & StateProps> = ({
+const View: FC<PropTypes & StateProps & DispatchProps> = ({
   pageRef,
   zoom,
+  pdfUrl,
   pageUrl,
+  nextPageFile,
+  nextNextPageFile,
+  cachedPages,
+  cachePage,
 }): ReactElement => {
   const docRef = useRef(null)
   const {
@@ -22,6 +29,25 @@ const View: FC<PropTypes & StateProps> = ({
     handleMouseReset,
     handlePan,
   } = usePanhandler(docRef, zoom)
+
+  useEffect(() => {
+    // Load next 2 pages to be loaded if not cached yet
+    // on p1 -> load 2, 3
+    // on p2 -> load 4, 5
+    // on p3 -> load 6, 7, etc.
+
+    if (nextPageFile && !cachedPages[nextPageFile]) {
+      const nextPage = new Image()
+      nextPage.src = `${pdfUrl}/${nextPageFile}`
+      cachePage(nextPageFile)
+    }
+
+    if (nextNextPageFile && !cachedPages[nextNextPageFile]) {
+      const nextNextPage = new Image()
+      nextNextPage.src = `${pdfUrl}/${nextNextPageFile}`
+      cachePage(nextNextPageFile)
+    }
+  }, [pdfUrl, nextPageFile, nextNextPageFile])
 
   return (
     <DocumentContainer ref={docRef}>
@@ -43,15 +69,33 @@ const View: FC<PropTypes & StateProps> = ({
 type StateProps = {
   zoom: number
   pageUrl: string
+  nextPageFile: string
+  nextNextPageFile: string
+  pdfUrl: string
+  cachedPages: PageCache
 }
 
 const mapStateToProps = ({
   zoom,
-  pages: { pages, currentPage },
+  pages: { pages, currentPage, cached },
   room: { pdfUrl },
 }): StateProps => ({
   zoom,
+  pdfUrl,
   pageUrl: `${pdfUrl}/${pages[currentPage - 1]}`,
+  nextPageFile: pages[currentPage + currentPage - 1],
+  nextNextPageFile: pages[currentPage + currentPage],
+  cachedPages: cached,
 })
 
-export default connect(mapStateToProps)(View)
+type DispatchProps = {
+  cachePage(page: string): void
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  cachePage(page): void {
+    dispatch(cachePage(page))
+  },
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(View)
