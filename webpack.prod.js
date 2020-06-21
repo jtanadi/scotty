259bf6a02b9ff3e30e09ac3254734c9f50c66ab9
Jesen Tanadi <mail@jesentanadi.com>
2020-06-21 11:04:58 -0400

    split chunks and compress

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const merge = require("webpack-merge")
 const TerserPlugin = require("terser-webpack-plugin")
+const CompressionPlugin = require("compression-webpack-plugin")
 
 const common = require("./webpack.common")
 
@@ -8,7 +9,15 @@ module.exports = merge(common, {
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
   },
+  plugins: [
+    new CompressionPlugin({
+      test: /\.[jt]sx*$/,
+    }),
+  ],
   resolve: {
     extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
   },
